7dbe164b630ab7614e8257934545a681d4d001ec
deidata <vova.kuznetsov332@gmail.com>
2026-09-22 15:12:13 +0300

    feat: выполнена задача 2

---
+++ practice-01/js/types.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const result_1 = "8" + 2;
+console.log("Результат 1:", result_1);
+console.log("Тип результата 1:", typeof result_1);
+
+const result_2 = "8" - 2;
+console.log("Результат 2:", result_2);
+console.log("Тип результата 2:", typeof result_2);
+
+const result_3 = Number("8") + 2;
+console.log("Результат 3:", result_3);
+console.log("Тип результата 3:", typeof result_3);
+
+const result_4 = "12" > "3";
+console.log("Результат 4:", result_4);
+console.log("Тип результата 4:", typeof result_4);
+
+const result_5 = 12 === "12";
+console.log("Результат 5:", result_5);
+console.log("Тип результата 5:", typeof result_5);
+
+const result_6 = Number("");
+console.log("Результат 6:", result_6);
+console.log("Тип результата 6:", typeof result_6);
+
+const result_7 = Number("text");
+console.log("Результат 7:", result_7);
+console.log("Тип результата 7:", typeof result_7);
+
+const result_8 = Boolean("false");
+console.log("Результат 8:", result_8);
+console.log("Тип результата 8:", typeof result_8);
+
+const result_9 = null;
+console.log("Результат 9:", result_9);
+console.log("Тип результата 9:", typeof result_9);
+
+const result_10 = NaN;
+console.log("Результат 10:", result_10);
+console.log("Тип результата 10:", typeof result_10);
